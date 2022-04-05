9e35ffb566a2d599a7c3069acdfc68104a2642a5
ignaciodamiang-y <ignaciodamiang@yahoo.com.ar>
2022-04-05 18:01:58 -0300

    app console

---
+++ NewApp.Console/NewApp.Console/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace NewApp.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Bienvenido a la calculadora!");
+            System.Console.WriteLine("Ingrese los numeros a sumar");
+
+            System.Console.WriteLine("Num 1: ");
+            string num1 = System.Console.ReadLine();
+
+            /*if (!Int32.TryParse(num1, out int int1))
+            {
+                System.Console.WriteLine("Ingrese un número válido: ");
+            }*/
+
+            System.Console.WriteLine("Num 2: ");
+            string num2 = System.Console.ReadLine();
+
+            /*if (!Int32.TryParse(num2, out int int2))
+            {
+                System.Console.WriteLine("Ingrese un número válido: ");
+            }*/
+
+
+            int int1 = Int32.Parse(num1);
+            int int2 = Convert.ToInt32(num2);
+
+            System.Console.WriteLine($"La suma de {int1} y {int2} es igual a {int1 + int2}");
+            System.Console.ReadKey();
+        }
+    }
+}
